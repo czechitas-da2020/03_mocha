538435a0adbdf25bde33ac61d87fe3e1356136a2
Vojtěch Červený <cervik49@gmail.com>
2020-11-14 10:36:08 +0100

    part of testcases

--- test/ukoly/task3.js
+++ test/ukoly/task3.js
@@ -11,15 +11,63 @@
 */
 
 describe('Automation page Adding', () => { 
+  let buttonAdd, cat, cats, buttonApokalypsa, buttonRemove, counter
   before(()=>{
     browser.url('https://automation.cervik.repl.co/adding.html')
+    buttonAdd = $('#addItem')
+    cat = $('.cat.card')
+    cats = $$('.cat.card')
+    buttonApokalypsa = $('#removeAll')
+    buttonRemove = $('#removeItem')
+    counter = $('#counter')
+  })
+  afterEach(()=>{
+    buttonApokalypsa.click()
   })
 
-  it('ověřím, že kliknutím tlačítka Přidej kočku se přidá kočka do seznamu', ()=>{})
-  it('ověřím, že multiple kliknutím tlačítka Přidej kočku se přidá kočky do seznamu')
-  it('ověř, že kliknutí tlačítka Odeber kočku odebere právě jednu kočku')
-  it('ověř, že tlačítko Apokalypsa smaže všechny karty')
-  it('Ověř, že počítadlo koček reaguje na přidání kočky, smazání kočky, smazání všech koček')
+  it('ověřím, že kliknutím tlačítka Přidej kočku se přidá kočka do seznamu', ()=>{
+    buttonAdd.click()
+    expect(cat).toBeDisplayed()
+  })
+  it('ověřím, že multiple kliknutím tlačítka Přidej kočku se přidá kočky do seznamu', ()=>{
+    for (let i = 0; i < 5; i++) {
+      buttonAdd.click()   
+    }
+    expect(cats).toBeElementsArrayOfSize(5)
+  })
+
+  it('ověř, že kliknutí tlačítka Odeber kočku odebere právě jednu kočku', ()=>{
+    buttonAdd.click()
+    expect(cats).toBeElementsArrayOfSize(1)
+    buttonRemove.click()
+    
+  })
+  it('ověř, že tlačítko Apokalypsa smaže všechny karty', ()=>{
+    for (let i = 0; i < 5; i++) {
+      buttonAdd.click()   
+    }
+    expect(cats).toBeElementsArrayOfSize(5)
+    buttonApokalypsa.click()
+    expect(cats).toBeElementsArrayOfSize(0)
+  })
+  describe('Pocitadlo', ()=>{
+    it('overim, ze kliknutim na tlaciko Pridej zvedne pocitadlo o 1', ()=>{
+      buttonAdd.click()
+      expect(counter).toHaveText('1')
+    })
+    it('overim, ze kliknutim na tlaciko Odeber zmensi pocitadlo o 1', ()=>{
+      buttonAdd.click()
+      buttonRemove.click()
+      expect(counter).toHaveText('0')
+    })
+    it('overim, ze kliknutim na tlaciko Apokalypsa nastavi hodnotu na 0', ()=>{
+      for (let i = 0; i < 5; i++) {
+        buttonAdd.click()   
+      }
+      buttonApokalypsa.click()
+      expect(counter).toHaveText('0')
+    })
+  })
   it('Ověř, že tlačítka na odebrání jsou aktivní jen, když je zobrazena aspoň jedna karta kočky')
   it('Ověř, že tlačítka na odebrání jsou deaktivována po smazání poslední karty kočky.')
 })

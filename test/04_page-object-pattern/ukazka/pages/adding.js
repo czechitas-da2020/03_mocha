1759b8c7175c2eed69364a915128edbdd42e021c
Vojtěch Červený <cervik49@gmail.com>
2021-05-03 22:39:55 +0200

    test: :fire: Update of some parts

--- test/04_page-object-pattern/ukazka/pages/adding.js
+++
@@ -1,8 +0,0 @@
-const addingPage = {
-  get buttonAddCat() { return $('#addItem') },
-  get buttonRemoveOne() { return $('#removeItem') },
-  get buttonRemoveAll() { return $('#removeAll') },
-  get cats() { return $$('.cat') },
-}
-
-module.exports = addingPage

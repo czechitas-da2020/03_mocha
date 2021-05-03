4be805a86d2637639dac94c4f6463b44882e3ab2
Vojtěch Červený <cervik49@gmail.com>
2021-05-03 23:13:42 +0200

    chore: change chromedriver version

--- test/ukoly/task1.js
+++ test/ukoly/task1.js
@@ -29,7 +29,7 @@ describe('03 Elements', () => {
         for (let i = 0; i < 10; i = i + 1) {
             // element, ktery jsme nasli na radku 12 a ted na nej klikame
             giveLikeButton.click()
-            browser.pause(500)
+            browser.pause(250)
         }
 
         expect(likeCounter).toHaveText('10') // overi, ze pocitadlo ukazuje spravnou hodnotu - 10
